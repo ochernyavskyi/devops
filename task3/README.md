8b75052f3cd104cce2616661a705eb4f9394548a
Cherniavskyi Oleh <ochernyavskyi@gmail.com>
2021-06-18 18:44:49 +0300

    Updated task3
    -Finished telegram bot
    -Added README for telegram bot

---
+++ task3/README.md
@@ -0,0 +1,14 @@
+<h1>Telegram bot<h1>
+<p>Hi everyone, in this folder U can find the code of telegram bot who is dealing with github.
+<p>All U need to change in the code - it's bot token.
+<p>The functionality of bot is following:
+<ul>
+<li>/help - will show U all available commands of bot</li>
+<li>/homerep - will give U the link for my repository</li>
+<li>/tasks - will show U the name with links for all completed tasks</li>
+<li>/task# (# - is a number of task) - will give U the link for this task</li>
+</ul> 
+
+Curently U can test the bot by this address in telegram: @ChernyavskyiDevOpsBot.
+
+<p>Enjoy :)
--- task3/main.go
+++ task3/main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"log"
-    "strconv"
-//     "regexp"
 	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"log"
+	"regexp"
+	"strconv"
 )
 
 func main() {
-	bot, err := tgbotapi.NewBotAPI("")
-	tasks := [...]string{"Ansible assignment", "Bash script"}
-	links := [...]string{"https://github.com/ochernyavskyi/devops/tree/master/task1", "https://github.com/ochernyavskyi/devops/tree/master/task2"}
-// 	var currentTask = regexp.MustCompile(`^task#.`)
+	bot, err := tgbotapi.NewBotAPI("Your bot token")
+	tasks := [...]string{"Ansible assignment", "Bash script", "Telegram bot", "Bash script with github API"}
+	links := [...]string{"https://github.com/ochernyavskyi/devops/tree/master/task1", "https://github.com/ochernyavskyi/devops/tree/master/task2", "https://github.com/ochernyavskyi/devops/tree/master/task3", "https://github.com/ochernyavskyi/devops/tree/master/task4"}
+	var commandPat = regexp.MustCompile(`^task[0-9]`)
 
 	if err != nil {
 		log.Panic(err)
@@ -32,27 +32,34 @@ func main() {
 		}
 		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
-
-if update.Message.IsCommand() {
+		if update.Message.IsCommand() {
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-			switch update.Message.Command() {
-			case "help":
-				msg.Text = "type /sayhi or /status."
-			case "homerep":
-				msg.Text = "https://github.com/ochernyavskyi/devops :)"
-			case "tasks":
-			    msg.ParseMode = "html"
-			    msg.Text = "The list of completed tasks:\n"
-			    for i := 0; i < len(tasks); i++ {
-       				msg.Text =  msg.Text + strconv.Itoa(i+1) + ". " + tasks[i] +  "  -  " + "<a href='"+links[i] +"'> <b>link</b></a>" + "\n"
-                }
-			case update.Message.Command == "^task#.":
+			switch cmd := update.Message.Command(); {
+			case cmd == "help":
 				msg.ParseMode = "html"
-				msg.Text = "This is test"
+				msg.Text = "The commands of this bot are following:\n /homerep - display home repository\n /tasks - display the list of all tasks\n /task# - (# is a number of task) will give U the link for this task."
+			case cmd == "homerep":
+				msg.Text = "https://github.com/ochernyavskyi/devops :)"
+			case cmd == "tasks":
+				msg.ParseMode = "html"
+				msg.Text = "The list of completed tasks:\n"
+				for i := 0; i < len(tasks); i++ {
+					msg.Text = msg.Text + strconv.Itoa(i+1) + ". " + tasks[i] + "  -  " + "<a href='" + links[i] + "'> <b>link</b></a>" + "\n"
+				}
+			case commandPat.MatchString(cmd):
+				cmd := cmd[4:5]
+				taskIndex, _ := strconv.Atoi(cmd)
+				if taskIndex <= len(links) {
+					for i := taskIndex; i <= (taskIndex); i++ {
+						msg.Text = links[i-1]
+					}
+				} else {
+					msg.Text = "No such task or it is not ready"
+				}
 			default:
 				msg.Text = "I don't know that command"
 			}
 			bot.Send(msg)
 		}
-}
+	}
 }
