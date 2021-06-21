5a64a8fc3790939f0f455b56f25a36d04fb0296b
Cherniavskyi Oleh <ochernyavskyi@gmail.com>
2021-06-21 19:37:02 +0300

    Updated task4
    -Finished to work with task4

--- task4/myscript.sh
+++ task4/myscript.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 
 echo "This is script which will deal with github API"
-read -p "Enter the interested github username and repository name (example: https://github.com/testuser/myrep: " input_data
+read -p "Enter the github url (example: https://github.com/testuser/myrep: " input_data
+input_data=${input_data,,}
 input_data=${input_data#https://github.com/}
 echo ${input_data}
 
 function pull_request_exist {
   echo "This function shows, the names of pull requests, if they are available"
-  curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/${input_data}/pulls | jq .[].title
+  curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/${input_data}/pulls?per_page=100 | jq '.[].title'
 }
 
 function contributors {
   echo "This function will print the list of most productive contributors, which have more than 1 open PR"
-  curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/${input_data}/pulls?state=opened | jq '.[].user.login' | uniq -cd
+  curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/${input_data}/pulls?state=opened?per_page=100 | jq '.[].user.login' | uniq -cd
 }
 
 function pull_request_count {
   echo "This function will print the number of PRs each contributor has created with the labels"
-  tmp="$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/alpinejs/alpine/pulls?state=opened | jq length)"
-  echo $tmp
-
+  contributor_lables=($(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/${input_data}/pulls | jq '.[] | .labels | length'))
+  contributor_names=($(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/${input_data}/pulls | jq '.[].user.login'))
+  echo
+  for ((i = 0; i < ${#contributor_names[@]}; i++)); do
+    echo "${contributor_names[i]} : ${contributor_lables[i]}"
+  done
 }
 
 function custom_func {
   echo "This function will show U all public repositories of user"
   username=${input_data%/*}
   echo $username
-  curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/users/${username}/repos | jq -r '.[] | .name'
+  curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/users/${username}/repos?per_page=100 | jq -r '.[] | .name'
 }
 
 PS3="Select the operation: "
